3dc8b04d295ea2bde0d1cca69004a6629e9ee234
Dani170907 <daniramadhan170907@gmail.com>
2025-01-14 20:55:15 +0700

    improve delay

--- PHP/aboutYou.php
+++ PHP/aboutYou.php
@@ -16,16 +16,16 @@ function singLyric($lyric, $initialDelay, $charDelay) {
 
 function singSong() {
     $lyrics = [
-        ["Do you think I have forgotten?", 300000, 100000],
-        ["Do you think I have forgotten?", 1300000, 100000],
-        ["Do you think I have forgotten", 1400000, 110000],
-        ["about you?", 2000000, 250000],
-        ["There was something bout you that now I can't remember", 3000000, 76000],
-        ["It's the same damn thing that made my heart surrender", 500000, 75000],
-        ["And I miss you on a train, I miss you in the morning", 500000, 80000],
-        ["I never know what to think about", 400000, 100000],
-        ["I think about youuuuuuuuuuuuuuuuuuuuuuuuuuu", 500000, 100000],
-        ["About you", 500000, 100000]
+        ["Do you think I have forgotten?", 300000, 110000],
+        ["Do you think I have forgotten?", 1200000, 110000],
+        ["Do you think I have forgotten", 1200000, 110000],
+        ["about you?", 1800000, 260000],
+        ["There was something bout you that now I can't remember", 2900000, 75000],
+        ["It's the same damn thing that made my heart surrender", 490000, 75000],
+        ["And I miss you on a train, I miss you in the morning", 400000, 80000],
+        ["I never know what to think about", 500000, 100000],
+        ["I think about youuuuuuuuuuuuuuuuuuuuu", 500000, 110000],
+        ["About you", 900000, 160000]
     ];
 
     foreach ($lyrics as $line) {
